0752cd4854c0bf6797282649b8f60fad5d78133b
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-04-26 12:40:25 +0530

    feat : MarketDatapoints lambda functions

--- utils/marketData.py
+++ utils/marketData.py
@@ -1,40 +1,11 @@
+import binance
+
 get_depth = lambda client,token_symbol: client.get_order_book(symbol=token_symbol)
-get_trades = lambda client,token_symbol: client.get_recent_trades(symbol=token_symbol)
-get_trades = lambda client,token_symbol: client.get_historical_trades(symbol=token_symbol)
-get_trades = lambda client,token_symbol: client.get_aggregate_trades(symbol=token_symbol)
+get_recent_trades = lambda client,token_symbol: client.get_recent_trades(symbol=token_symbol)
+get_historical_trades = lambda client,token_symbol: client.get_historical_trades(symbol=token_symbol)
+get_aggregate_trades = lambda client,token_symbol: client.get_aggregate_trades(symbol=token_symbol)
+get_agg_trades = lambda client,token_symbol,start_str : client.aggregate_trade_iter(symbol=token_symbol, start_str=start_str) 
 
-agg_trades = client.aggregate_trade_iter(symbol='ETHBTC', start_str='30 minutes ago UTC')
+candles = lambda client,token_symbol,interval: client.get_klines(symbol=token_symbol, interval=Client.interval)
+klines = lambda client,token_symbol,interval,start_str: client.get_historical_klines(token_symbol, Client.interval,start_str) 
 
-# iterate over the trade iterator
-for trade in agg_trades:
-    print(trade)
-    # do something with the trade data
-
-# convert the iterator to a list
-# note: generators can only be iterated over once so we need to call it again
-agg_trades = client.aggregate_trade_iter(symbol='ETHBTC', '30 minutes ago UTC')
-agg_trade_list = list(agg_trades)
-
-# example using last_id value
-agg_trades = client.aggregate_trade_iter(symbol='ETHBTC', last_id=23380478)
-agg_trade_list = list(agg_trades)
-candles = client.get_klines(symbol='BNBBTC', interval=Client.KLINE_INTERVAL_30MINUTE)
-
-# fetch 1 minute klines for the last day up until now
-klines = client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
-
-# fetch 30 minute klines for the last month of 2017
-klines = client.get_historical_klines("ETHBTC", Client.KLINE_INTERVAL_30MINUTE, "1 Dec, 2017", "1 Jan, 2018")
-
-# fetch weekly klines since it listed
-klines = client.get_historical_klines("NEOBTC", Client.KLINE_INTERVAL_1WEEK, "1 Jan, 2017")
-
-for kline in client.get_historical_klines_generator("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
-    print(kline)
-    # do something with the kline
-avg_price = client.get_avg_price(symbol='BNBBTC')
-tickers = client.get_ticker()
-
-prices = client.get_all_tickers()
-
-tickers = client.get_orderbook_tickers()
